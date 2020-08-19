6274766d9d29a18d2e4203c7472e1f1be0a1e8a3
Gutemberg Braga <gutbra.arag@hotmail.com>
2020-08-19 17:58:01 -0300

    listar produtos

---
+++ src/main/java/br/com/rdevs/ecommerce/produto/model/entity/TbTipoProduto.java
@@ -0,0 +1,24 @@
+package br.com.rdevs.ecommerce.produto.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "TB_TIPO_PRODUTO")
+@Data
+public class TbTipoProduto {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_TIPO_PRODUTO")
+    @Id
+    private Long idTipoProduto;
+
+    @Column(name = "DS_TIPO_PRODUTO")
+    private String dsTipoProduto;
+
+    @OneToMany(mappedBy = "tipoProduto", cascade = CascadeType.ALL)
+    private List<TbProduto> produtos;
+}
